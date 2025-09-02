35c3a042f21ce71bc4418f5caa1e9bfb6d78f346
Anupam Rabha <220020007@iitdh.ac.in>
2025-09-02 17:05:18 +0530

    Task1- only blinking the red led

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+#define STCTRL      (*((volatile uint32_t *)0xE000E010))
+#define STRELOAD    (*((volatile uint32_t *)0xE000E014))
+#define STCURRENT   (*((volatile uint32_t *)0xE000E018))
+
+#define RED_LED   (1U << 1)
+#define ENABLE    (1U << 0)
+#define CLK_SRC   (1U << 2)
+#define COUNTFLAG (1U << 16)
+
+int main(void) {
+    // Enable PORTF clock
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    GPIO_PORTF_DIR_R |= RED_LED;
+    GPIO_PORTF_DEN_R |= RED_LED;
+
+
+    STRELOAD = (80000 * 1000) - 1;
+    STCTRL = CLK_SRC;
+
+    while (1) {
+        STCURRENT = 0;       // clear timer
+        STCTRL |= ENABLE;
+
+
+        while ((STCTRL & COUNTFLAG) == 0);
+
+        GPIO_PORTF_DATA_R ^= RED_LED;  // Toggle LED
+    }
+}
